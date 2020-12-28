3f65f7919f09e26ac1060336c5fb6bbf0f45d22f
aliasanuj <Writetoanuz@gmail.com>
2020-12-28 21:39:36 +0530

    onlyDemo.pyFile

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 print("This is demo File")
 print("This is second line")
 
-def calc_sum(a,b):
-    return a+b
-
-def calc_multiply(a,b):
-    return a*b
+# def calc_sum(a,b):
+#     return a+b
+#
+# def calc_multiply(a,b):
+#     return a*b
 
